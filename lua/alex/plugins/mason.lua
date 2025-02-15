11f68a28e419c9730675e0ebdfff18252385132c
PolarTango-Laptop <GameRampol@yandex.ru>
2025-02-15 14:32:18 +0300

    some configs for lsp and better view

--- lua/alex/plugins/mason.lua
+++ lua/alex/plugins/mason.lua
@@ -1,8 +1,21 @@
 return {
   {
     "williamboman/mason.nvim",
+    dependencies = 'WhoIsSethDaniel/mason-tool-installer.nvim',
     config = function()
       require("mason").setup()
+      require("mason-tool-installer").setup {
+	'gopls',
+	'delve'
+      }
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup {
+	ensure_installed = { "lua_ls" }
+      }
     end
   }
 }
--- lua/alex/plugins/telescope.lua
+++ lua/alex/plugins/telescope.lua
@@ -1,5 +1,9 @@
 return {
   'nvim-telescope/telescope.nvim',
   tag = '0.1.8',
-  dependencies = { 'nvim-lua/plenary.nvim' }
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function ()
+    local telescope = require("telescope")
+    telescope.load_extension("noice")
+  end
 }
