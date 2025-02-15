de7e0417378878c870561d34f3afce67ee7bc7f7
PolarTango-Laptop <GameRampol@yandex.ru>
2025-02-15 17:05:49 +0300

    change blink.cmp

--- lua/alex/plugins/blink.lua
+++ lua/alex/plugins/blink.lua
@@ -1,20 +1,24 @@
 return {
-  {
-    'saghen/blink.cmp',
-    event = { 'lspAttach' },
-    dependencies = {
-      'rafamadriz/friendly-snippets',
-    },
-    version = '*',
+  'saghen/blink.cmp',
+  --enabled = false,
+  --event = { 'lspAttach' },
+  dependencies = {
+    'rafamadriz/friendly-snippets',
+  },
+  version = '*',
 
-    opts = {
-      keymap = { preset = 'default' },
-      appearance = {
-	use_nvim_cmp_as_default = true,
-	nerd_font_variant = 'mono'
-      },
+  opts = {
+    keymap = { preset = 'default' },
+    appearance = {
+      use_nvim_cmp_as_default = true,
+      nerd_font_variant = 'mono'
     },
-    documentation = { auto_show = true, auto_show_delay_ms = 200 },
-    snippets = { preset = 'default' },
-  }
+    completion = {
+      menu = { border = 'single' },
+      documentation = { window = { border = 'single' } },
+      list = { max_items = 200 },
+    },
+  },
+  documentation = { auto_show = true, auto_show_delay_ms = 200 },
+  snippets = { preset = 'default' },
 }
